8ceb3f5a2045d56160f5e13630fbe191bff6256b
Jaosou <89335399+Jaosou@users.noreply.github.com>
2025-04-10 22:56:59 +0700

    Fig vercel test

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
 import Navbar from "@/components/Navbar/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Provider>
           <Navbar/>
           {children}
+          <SpeedInsights/>
         </Provider>
       </body>
     </html>
